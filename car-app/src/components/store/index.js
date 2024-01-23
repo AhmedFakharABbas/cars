3707ce5595d79912daeae3361cfe5db895672cdb
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2024-01-23 22:09:54 +0500

    configured store

--- car-app/src/components/store/index.js
+++ car-app/src/components/store/index.js
@@ -1,12 +1,16 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 import {
-    carsReducer,
-addCar, 
-removeCar,
-changeSearchTerm
-} from './slices/carsSlice'
-import {
-formReducer,
-changeName,
-changeCost
-} from './slices/formSlice'
+  carsReducer,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+} from "./slices/carsSlice";
+import { formReducer, changeName, changeCost } from "./slices/formSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
+export { store, addCar, removeCar, changeSearchTerm, changeName, changeCost };
