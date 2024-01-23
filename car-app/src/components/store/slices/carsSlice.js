e718a83fd8c03a7a03624e36f0499c630c5ae5c0
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2024-01-23 21:58:39 +0500

    add carsSlice

---
+++ car-app/src/components/store/slices/carsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+const carSlice = createSlice({
+  initialState: {
+    searchTerm: "",
+    cars: [],
+  },
+  reducers: {
+    changeSearchTerm( state, action) {
+      state.searchTerm = action.payload;
+    },
+    //asumption  action payload is similar to like this {name :"sdfsd", cost:324234}
+    addCar( state, action) {
+        state.cars.push({
+            name:action.payload.name,
+            cost:action.payload.cost,
+            id:nanoid()
+        })
+    },
+    //asumption payload will have the id the car want to remove 
+    removeCar( state, action) {
+        const updatedCars=state.cars.filter((car)=>{
+            return car.id !=action.payload
+        })
+        state.cars=updatedCars
+    },
+  },
+});
+export const {changeSearchTerm, addCar, removeCar } =carSlice.actions;
