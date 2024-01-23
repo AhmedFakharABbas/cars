cf7dec601b896d162cc1a2bc669a1017f9bd39d3
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2024-01-23 23:53:09 +0500

    Updated store

--- car-app/src/components/CarForm.js
+++ car-app/src/components/CarForm.js
@@ -1,15 +1,15 @@
-import {useDispatch,useSelector} from 'react-redux'
-import {changeName} from './store'
+import {  useSelector, useDispatch } from "react-redux";
+import { changeName  } from "../store/index";
 
 function CarForm() {
-    const dispatch=useDispatch();
-    const name=useSelector((state)=>{
-        return state.name
-    })
-   const  handleNameChange=(event)=>{
+ const dispatch = useDispatch();
+  const name = useSelector((state) => {
+    return state.name;
+  });
+  const handleNameChange = (event) => {
+    console.log('value',event.target.value)
     dispatch(changeName(event.target.value));
-
-    };
+  };
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Add Car</h4>
@@ -18,9 +18,10 @@ function CarForm() {
           <div className="field">
             <label className="label"></label>
             <input
-            value={name}
-            onChange={handleNameChange}
-            className="input is-expanded"/>
+              value={name}
+              onChange={handleNameChange}
+              className="input is-expanded"
+            />
           </div>
         </div>
       </form>
