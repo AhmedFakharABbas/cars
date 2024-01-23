4559cca4f912a0aa4c74875b28ce738ae1bcd652
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2024-01-23 14:30:30 +0500

    Basic setup

---
+++ car-app/src/index.js
@@ -0,0 +1,8 @@
+import React  from "react";
+import {createRoot} from "react-dom/client"
+import App from './App'
+
+const el=document.getElementById('root')
+const root= createRoot(el)
+
+root.render(<App/>)
