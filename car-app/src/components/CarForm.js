6dcb0266a0c763a8dc0f96a3427129af5cac9343
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2024-01-23 22:42:45 +0500

    worked on car form

--- car-app/src/components/CarForm.js
+++ car-app/src/components/CarForm.js
@@ -1,4 +1,22 @@
-  function CarForm(){
-    return <div>CarForm</div>
+function CarForm() {
+   const  handleNameChange=(event)=>{
+
+    };
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label"></label>
+            <input
+            value={name}
+            onChange={handleNameChange}
+            className="input is-expanded"/>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 export default CarForm;
