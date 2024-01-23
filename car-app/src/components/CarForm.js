54581bcd0f8d7a1d0132c3ace42f0826344ebe41
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2024-01-23 23:26:32 +0500

    updated Form js

--- car-app/src/components/CarForm.js
+++ car-app/src/components/CarForm.js
@@ -1,5 +1,10 @@
+import {useDispatch} from 'react-redux'
+import {changeName} from './store'
+
 function CarForm() {
+    const dispatch=useDispatch();
    const  handleNameChange=(event)=>{
+    dispatch(changeName(event.target.value));
 
     };
   return (
